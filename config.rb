93c4a3d7ba1fe5750729cd3af1fa2cd50f895b6c
Draigan <draigan.lefebvre@gmail.com>
2022-12-11 21:54:38 -0500

    Created better variables and applied them

--- config.rb
+++ config.rb
@@ -37,7 +37,7 @@ data.projects.each do |p|
   proxy "/projects/#{p.slug}.html", "/projects/show.html", locals: { project: p } , :ignore => true
 end
 
-proxy "/skills.html", "/skills/index.html", locals: { bem_modifier_skill_wrapper: "-with-description" }, :ignore => true
+proxy "/skills.html", "/skills/index.html",  :ignore => true
 
 data.skills.each do |s|
   proxy "/skills/#{s.slug}.html", "/skills/show.html", locals: { skill: s } , :ignore => true
